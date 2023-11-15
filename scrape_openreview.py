00e18e925e1cf812e159efdaa0f5fb47a1385604
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2023-11-14 20:37:22 -0800

    add main(), dump dataq into csv

--- scrape_openreview.py
+++ scrape_openreview.py
@@ -3,16 +3,17 @@
 Scrape OpenReview for emails of the authors.
 Default conference is ICLR 2023.
 """
+import argparse
+import logging
 import re
-# import argparse
 
-import whois
+import pandas
 import requests
 from bs4 import BeautifulSoup
 
-# conference = "ICLR.cc"
-# year = 2023
+_LOG = logging.getLogger(__name__)
 
+# Scrape HTML for papers so we don't have to login.
 _URL_ROOT = "https://openreview.net"
 _URL_API = "https://api2.openreview.net"
 
@@ -22,27 +23,28 @@ _HTTP_TIMEOUT = 5  # seconds
 
 def get_venues(conference, year):
     """
-    Get the list of URLs of all conference's venues.
+    Get the list of all conference's venues.
     """
     response = requests.get(
         f"{_URL_ROOT}/venue?id={conference}", timeout=_HTTP_TIMEOUT)
     soup = BeautifulSoup(response.text, "html.parser")
     href_regex = re.compile(rf"/submissions\?venue={conference}/{year}/.*")
-    return [f"{_URL_ROOT}{a['href']}"
-            for a in soup.find_all("a", href=href_regex)]
+    return [a['href'] for a in soup.find_all("a", href=href_regex)]
 
 
-def get_authors(url_venue, max_pages=float("inf")):
+def get_authors(venue, max_pages=float("inf")):
     """
     Fetch all pages of the conference venue and extract the IDs of the authors.
     For each paper, yield a triplet of (author_id, author_index, num_authors).
     """
     page = 1
     while page <= max_pages:
-        response = requests.get(f"{url_venue}&page={page}", timeout=_HTTP_TIMEOUT)
+        _LOG.info("Get authors for venue: %s page: %d of %s", venue, page, max_pages)
+        response = requests.get(f"{_URL_ROOT}{venue}&page={page}", timeout=_HTTP_TIMEOUT)
         soup = BeautifulSoup(response.text, "html.parser")
         for paper in soup.find_all("div", {"class": "note-authors"}):
             num_authors = len(paper)
+            _LOG.info("Paper %s has %d authors", paper, num_authors)
             for (i, author) in enumerate((a['title'] for a in paper.find_all("a")), 1):
                 yield (author, i, num_authors)
         if max_pages is None:
@@ -60,11 +62,22 @@ def get_author_domain(author_id):
                for prof in profiles for pos in prof["content"]["history"])[2]
 
 
-def get_location(domain):
-    """
-    Get the country and state of the domain.
-    """
-    w = whois.whois(domain)
-    country = w.country or w.registrant_country
-    state = w.state or w.registrant_state
-    return (country, state)
+def _main():
+    parser = argparse.ArgumentParser(
+        description="Scrape OpenReview for locations of the authors.")
+    parser.add_argument("conference", default="ICLR.cc")
+    parser.add_argument("year", type=int, default=2023)
+    parser.add_argument("output", default=None, default=2023)
+    args = parser.parse_args()
+    venues = get_venues(args.conference, args.year)
+    _LOG.info("Venues:\n%s", "\n  * ".join(venues))
+    df = pandas.DataFrame(
+        data=(
+            (args.conference, args.year, venue, author, author_pos, num_authors)
+            for venue in venues
+            for (author, author_pos, num_authors) in get_authors(venue)
+        ),
+        columns=["conference", "year", "venue", "author", "author_pos", "num_authors"]
+    )
+    fname_output = args.output or f"{args.conference}_{args.year}.csv"
+    df.to_csv(fname_output, index=False)
