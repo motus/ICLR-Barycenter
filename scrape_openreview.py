4401b2b1df56e7c9cac4e7dda4ab406323bfc0b6
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2023-11-14 21:57:37 -0800

    fix the max_pages logic

--- scrape_openreview.py
+++ scrape_openreview.py
@@ -31,35 +31,43 @@ def get_venues(conference, year):
     return [a['href'] for a in soup.find_all("a", href=href_regex)]
 
 
-def get_authors(venue, max_pages=float("inf")):
+def get_authors(venue, max_pages=None):
     """
     Fetch all pages of the conference venue and extract the IDs of the authors.
     For each paper, yield a triplet of (author_id, author_index, num_authors).
     """
     page = 1
-    while page <= max_pages:
+    while max_pages is None or page <= max_pages:
+
         _LOG.info("Get authors for venue: %s page: %d of %s", venue, page, max_pages)
         response = requests.get(f"{_URL_ROOT}{venue}&page={page}", timeout=_HTTP_TIMEOUT)
         soup = BeautifulSoup(response.text, "html.parser")
+
         for (i, paper) in enumerate(soup.find_all("div", {"class": "note-authors"})):
-            num_authors = len(paper)
+            authors = paper.find_all("a")
+            num_authors = len(authors)
             _LOG.info("Paper %d has %d authors", i, num_authors)
-            for (author_idx, author) in enumerate((a['title'] for a in paper.find_all("a")), 1):
-                yield (author, author_idx, num_authors)
+            for (author_idx, author_id) in enumerate((a['title'] for a in authors), 1):
+                yield (author_id, author_idx, num_authors)
+
+        page += 1
         if max_pages is None:
             max_pages = int(soup.find_all("a", href=_RE_LAST_PAGE)[-1].text)
-        page += 1
+
 
 
 def _main():
+
     parser = argparse.ArgumentParser(
         description="Scrape OpenReview for IDs the authors.")
     parser.add_argument("--conference", default="ICLR.cc")
     parser.add_argument("--year", type=int, default=2023)
     parser.add_argument("--output", default=None)
     args = parser.parse_args()
+
     venues = get_venues(args.conference, args.year)
     _LOG.info("Venues:\n%s", "\n  * ".join(venues))
+
     df = pandas.DataFrame(
         data=(
             (args.conference, args.year, venue, author, author_pos, num_authors)
@@ -68,6 +76,7 @@ def _main():
         ),
         columns=["conference", "year", "venue", "author", "author_pos", "num_authors"]
     )
+
     fname_output = args.output or f"{args.conference}_{args.year}.csv"
     df.to_csv(fname_output, index=False)
 
