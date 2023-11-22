d7e39c1f1b5fe273845ff8804eb7406c27d984b6
Sergiy Matusevych <sergiym@microsoft.com>
2023-11-22 15:36:33 -0800

    working version of OpenAI script for getting the airport locations

--- scrape_locations.py
+++ scrape_locations.py
@@ -1,44 +1,65 @@
 #!/usr/bin/env python3
 """
-Get the domains and locations of the authors from OpenReview.
+Get the airport codes of the authors of the accepted papers.
 """
 import argparse
+import csv
+import json
 import logging
 
 import pandas
-import whois
+from openai import OpenAI
 
 _LOG = logging.getLogger(__name__)
 
 
-def get_location(domain):
+_OAI_MODEL = "gpt-3.5-turbo"
+_OAI_REQUEST = """
+    Good.
+    What international airport is the nearest to the {domain} largest campus?
+    Respond with 3-letter IATA code.
+"""
+
+
+def get_location(client, domain):
     """
-    Get the country and state of the domain.
+    Get the location of the domain.
     """
+    _LOG.debug("Domain: %s", domain)
     if domain is None:
-        return (None, None, None)
-    w = whois.whois(domain)
-    country = w.country or w.registrant_country
-    state = w.state or w.registrant_state
-    city = w.city or w.registrant_city
-    _LOG.info("Domain: %s Location: %s, %s, %s", domain, country, state, city)
-    return (country, state, city)
+        return None
+    chat_completion = client.chat.completions.create(
+        messages=[{
+            "role": "user",
+            "content": _OAI_REQUEST.format(domain=domain)
+        }],
+        model=_OAI_MODEL
+    )
+    response = chat_completion.choices[0].message.content
+    _LOG.info("Response for : %s :: %s", domain, response)
+    return response
 
 
 def _main():
     parser = argparse.ArgumentParser(
-        description="Use whois to get the locations of the authors' domains.")
+        description="Use ChatGPT to get the airports closest to the authors' domains.")
+    parser.add_argument("--config", help="JSON config with OpenAI API key")
     parser.add_argument("input")
     parser.add_argument("output")
     args = parser.parse_args()
+
+    with open(args.config, "rt", encoding="utf-8") as f:
+        config = json.load(f)
+
     df = pandas.read_csv(args.input)
-    domains = list(
-        df.groupby("domain").count().sort_values("author", ascending=False).index
-    )
-    locations = {domain: get_location(domain) for domain in domains}
-    df["country"] = df.domain.map(lambda d: locations[d][0])
-    df["state"] = df.domain.map(lambda d: locations[d][1])
-    df.to_csv(args.output, index=False)
+    domains = sorted(dom for dom in set(df.domain[df.accepted])
+                     if dom and isinstance(dom, str))
+
+    client = OpenAI(api_key=config["key"])
+    responses = [(dom, get_location(client, dom)) for dom in domains]
+
+    df_out = pandas.DataFrame(data=responses, columns=["domain", "response"])
+    df_out.to_csv(args.output, index=False, quoting=csv.QUOTE_ALL)
 
 
 if __name__ == "__main__":
